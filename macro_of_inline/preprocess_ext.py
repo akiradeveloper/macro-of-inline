ba66de0fa76b6e409efae3e8d81965730552a432
Akira Hayakawa <ruby.wktk@gmail.com>
2014-09-01 12:25:27 +0900

    Add comment
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- macro_of_inline/preprocess_ext.py
+++ macro_of_inline/preprocess_ext.py
@@ -38,13 +38,16 @@ def analyzeInclude(filename, txt):
 			current_result.append(line)
 	return result
 
-# Copied from pycparser (tests/test_c_generator.py)
-# Hope this comparator be in the library code.
 def compare_asts(ast1, ast2):
 	if type(ast1) != type(ast2):
 		return False
+
+	# macro-of-inline add namespace argument if it's calling macro.
+	# Thus, pure AST comparator doesn't work as the FuncCalls aren't equal
+	# if only one of them is as a result of rewrite.
 	if isinstance(ast1, c_ast.FuncCall) and isinstance(ast2, c_ast.FuncCall):
 		return True
+
 	if isinstance(ast1, tuple) and isinstance(ast2, tuple):
 		if ast1[0] != ast2[0]:
 			return False
