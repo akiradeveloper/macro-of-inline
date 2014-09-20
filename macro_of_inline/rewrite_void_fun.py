b823b84859820b88914c9296a30aa0a8a084ef8e
Akira Hayakawa <ruby.wktk@gmail.com>
2014-09-20 16:16:48 +0900

    Add guard if struct or union in visit_Decl
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- macro_of_inline/rewrite_void_fun.py
+++ macro_of_inline/rewrite_void_fun.py
@@ -66,6 +66,9 @@ class RenameVars(ext_pycparser.NodeVisitor):
 		self.revertTable()
 
 	def visit_Decl(self, node):
+		if isinstance(node.type, c_ast.Struct) or isinstance(node.type, c_ast.Union):
+			return
+
 		self.cur_table.register(node.name)
 		alias = self.cur_table.alias(node.name)
 		utils.P("Decl: %s -> %s" % (node.name, alias))
@@ -82,11 +85,9 @@ class RenameVars(ext_pycparser.NodeVisitor):
 		"""
 		self.visit(node.name)
 
-	# FIXME
-	# If the block has declartion of struct or union which is not anonymous
-	# then the first visit is to Decl node. In there, it rewrites the members.
 	def visit_Struct(self, n):
 		pass
+
 	def visit_Union(self, n):
 		pass
 
